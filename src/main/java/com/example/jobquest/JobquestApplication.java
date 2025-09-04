e0e78bc10dbbc634d6f00aef65e04efc87ed3030
shefali12-ab <kanaujiashefali@gmail.com>
2025-09-04 23:27:19 +0530

    initial folder structure added

---
+++ src/main/java/com/example/jobquest/JobquestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobquestApplication.class, args);
+	}
+
+}
